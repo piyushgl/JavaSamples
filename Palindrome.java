9d479523e54dd8966d6b91e94f2385eb16b1b772
piyushgl <59592007+piyushgl@users.noreply.github.com>
2020-01-19 10:27:12 -0500

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * to check whether its a palindrome or not
+ */
+
+class Palindrome {
+    static boolean checkPalindrome(int nbr) {
+
+        int rev = 0, temp = 0;
+        int orig = nbr;
+        while (nbr > 0) {
+            temp = nbr % 10;
+            rev = rev * 10 + temp;
+            nbr = nbr / 10;
+        }
+        if (orig == rev)
+            return true;
+        else
+            return false;
+    }
+
+    // Driver program to test above functions
+    public static void main(String[] args) {
+        int x = 9219;
+        System.out.println(checkPalindrome(x));
+        x = 1;
+        System.out.println(checkPalindrome(x));
+        x = 313;
+        System.out.println(checkPalindrome(x));
+    }
+}
