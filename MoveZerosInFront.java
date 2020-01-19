6915ace0f716a2b7479adbb991ca692af462cf7d
piyushgl <59592007+piyushgl@users.noreply.github.com>
2020-01-19 10:55:38 -0500

    MoveZerosInFront

---
+++ MoveZerosInFront.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class MoveZerosInFront {
+    static void allZeroInFront(int arr[], int len) {
+        int count = len - 1;
+        for (int i = len - 1; i >= 0; i--) {
+            if (arr[i] > 0) {
+                arr[count--] = arr[i];
+                System.out.println(Arrays.toString(arr));
+            }
+
+        }
+        while (count >= 0)
+            arr[count--] = 0;
+        System.out.print(Arrays.toString(arr));
+
+    }
+
+
+    // Driver program to test above functions
+    public static void main(String[] args) {
+        int arr1[] = {1, 0, 45, 0, 10, 19};
+        allZeroInFront(arr1, arr1.length);
+
+
+    }
+}
