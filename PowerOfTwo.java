b369a03e961587489a534d5942ccec06757148bf
piyushgl <59592007+piyushgl@users.noreply.github.com>
2020-01-19 09:54:19 -0500

    PowerOfTwo fixed

---
+++ PowerOfTwo.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * Program to check whether a number is a power of 2 or not
+ *
+ */
+
+class PowerOfTwo {
+    static void checkIfPowerOfTwo(int nbr) {
+
+        while (nbr % 2 == 0) {
+            nbr = nbr / 2;
+            if (nbr == 1) {
+                System.out.println("Power of 2");
+                System.exit(0);
+            }
+        }
+        System.out.println("Not a Power of 2");
+    }
+
+    // Driver program to test above functions
+    public static void main(String[] args) {
+        int x = 16;
+        checkIfPowerOfTwo(x);
+    }
+}
