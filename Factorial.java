b6a0041fda4001ed52509fc0fa3c219e66d48d26
piyushgl <59592007+piyushgl@users.noreply.github.com>
2020-01-19 10:30:48 -0500

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+/*
+ * To find factorial
+ */
+
+class Factorial {
+    static int factorial(int nbr) {
+        if (nbr == 1)
+            return 1;
+        else
+            return nbr * factorial(nbr - 1);
+    }
+
+
+    // Driver program to test above functions
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println(factorial(x));
+    }
+}
